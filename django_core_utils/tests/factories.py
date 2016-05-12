806758d7e32780d83dcfe9f10b85ef0669a655f1
Amnon Janiv <amnon.janiv@ondalear.com>
2016-05-12 19:51:09 -0400

    add GroupFactory

--- django_core_utils/tests/factories.py
+++ django_core_utils/tests/factories.py
@@ -10,7 +10,7 @@ import uuid as _uuid
 from random import randint
 
 import factory
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.sites.models import Site
 
 from python_core_utils.core import class_name
@@ -33,7 +33,7 @@ def model_class_name(factory_class):
 def default_name(cls, number):
     """Return a default name for given class.
     """
-    return '{}_{}'.format(class_name(cls), number)
+    return "{}_{}".format(class_name(cls), number)
 
 
 class UserFactory(factory.DjangoModelFactory):
@@ -42,9 +42,9 @@ class UserFactory(factory.DjangoModelFactory):
     class Meta(object):
         """Model meta class."""
         model = User
-        django_get_or_create = ('username',)
+        django_get_or_create = ("username",)
 
-    username = 'test_user'
+    username = "test_user"
 
     @staticmethod
     def create_users(count=1, prefix='test_user'):
@@ -52,6 +52,17 @@ class UserFactory(factory.DjangoModelFactory):
         users = [UserFactory(username='%s_%d' % (prefix, index))
                  for index in range(1, count + 1)]
         return users
+
+
+class GroupFactory(factory.DjangoModelFactory):
+    """Group factory class."""
+
+    class Meta(object):
+        """Model meta class."""
+        model = Group
+        django_get_or_create = ("name",)
+
+    name = "test_group"
 
 
 class SiteFactory(factory.DjangoModelFactory):
